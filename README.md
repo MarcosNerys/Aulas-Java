30800bfef58f32bc88ecdf03618e4845de8b0bf5
SocramDevNerys <devsocramnerys@outlook.com>
2025-03-27 11:12:41 -0300

    change names

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Aulas-Java
 Site de hopesdagem de códigos de estudo.
-
 <!-- Este Repositório tem a finalidade de salvar minhas aulas e códigos de Estudos -->
--- aula-um/src/Marcos.java
+++ aula-um/src/Marcos.java
@@ -1,4 +1,4 @@
-public class App {
+public class Marcos {
     public static void main(String[] args) throws Exception {
         System.out.println("Primeiro Código");
     }
